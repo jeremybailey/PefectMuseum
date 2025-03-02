8a396a974cf230e790b4e6517f0a6aba3436559f
jeremybailey <baileyorama@gmail.com>
2025-03-02 14:38:08 -0700

    56 compatible

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import {
     } from '@snap/camera-kit'
 
     (async function(){
-    var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjY0NzQ4ODkzLCJzdWIiOiJjYmM3NTUxOS1hYTAzLTRiZDMtOTIzMi04NjA3ZDU5YzE3Zjl-U1RBR0lOR35hYzlhYTRhNC1iODUzLTQ1OWItOWJjNS04ODExYzM4N2VkNzIifQ.r4YbsWJp4kRUPAkokn2naKXDt8cMaqXurg02gyVqVvA' })
+    var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjY0NzQ4ODkzLCJzdWIiOiJjYmM3NTUxOS1hYTAzLTRiZDMtOTIzMi04NjA3ZDU5YzE3Zjl-UFJPRFVDVElPTn4xMmFhNjZkZC0zZjdjLTQ4ZDgtYTc2NC1kMjljYWRmYmEyNGYifQ.jADIZpq3Lr9T_7dMXYjU-HG9Z40KiiL7cG4Dgp862hA' })
 
     const session = await cameraKit.createSession()
     document.getElementById ('canvas').replaceWith (session.output.live)
